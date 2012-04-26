7d5ae78ea4b4fc0143fd1bd5aa4e41150cfd16df
colinmccann <colindmccann@gmail.com>
2012-04-26 18:30:31 -0400

    cleaned up and working wall skeleton

--- js/washago.wall.js
+++ js/washago.wall.js
@@ -1,10 +1,67 @@
 /*jshint browser: true, devel: true */
-/*globals Sail, jQuery */
+/*globals Sail, jQuery, _ */
 var Washago = window.Washago || {};
 
 Washago.Wall = (function() {
     var self = {};
-    var tagArray = [];
+    self.cumulativeTagArray = [];
+
+    var createBalloon = function(contribution) {
+        // this function creates the balloon, adds the text, positions it on the board
+
+        var balloon = jQuery("<div class='balloon contribution'></div>");
+
+        balloon.attr('id', "contibution-" + contribution.id);
+        balloon.addClass('author-' + contribution.author);
+        jQuery(contribution.tags).each(function() {
+            balloon.addClass(this);
+        });
+
+        balloon.hide(); // initially hidden, we call show() with an effect later
+
+        // whole mess of stuff goes here (formatting, adding contribution.text, positioning, dragging, etc.)
+
+        jQuery("#board").append(balloon);
+        balloon.show('puff', 'fast');
+
+        return balloon;
+    };
+
+    var updateTagList = function(contribution) {
+        // this function handles the UI stuff for the list of tags on the sidebar
+        // then updates the list of user created tags with newly submitted tags
+
+        var none_yet = jQuery('#tags .none-yet');
+        if (none_yet.length > 0) {
+            none_yet.remove();
+        }
+        
+        var list = jQuery('#tags ul');
+        _.each(contribution.tags, function (tag) {
+            //klass = CommonBoard.keywordToClassName(tag);
+            var li = list.find('.' + tag);                          // what's going on here?
+            if (li.length === 0) {
+                li = jQuery('<li></li>'); li.text(tag);
+                li.addClass("tag-" + tag);
+                li.click(function() {
+                    // TODO set this up for filtering
+                    //self.toggleTag(tag);
+                });
+                list.append(li);
+            }
+        });
+
+
+        // _.difference(array, *others) 
+        // _.difference([1, 2, 3, 4, 5], [5, 2, 10]);
+        // => [1, 3, 4]
+        // update the saved list of tags
+        self.cumulativeTagArray = _.difference(contribution.tags, self.cumulativetagArray);
+    };
+
+    var writeToDB = function (contribution) {
+        alert("I'm writing to a non-existent DB!");
+    };
 
     self.init = function() {
         Sail.app.groupchatRoom = 'washago@conference.' + Sail.app.xmppDomain;
@@ -41,7 +98,7 @@ Washago.Wall = (function() {
 
         sail: {
             new_contribution: function(sev) {
-                contribution = {
+                var contribution = {
                     author:sev.payload.author,
                     text:sev.payload.text,
                     tags:sev.payload.tags,
@@ -49,69 +106,13 @@ Washago.Wall = (function() {
                     disourceType:sev.payload.discourse_type,
                     timestamp:sev.timestamp,
                     id:sev.payload.id
-                }
-                self.createBalloon(contribution)
-                self.updateTagList(contribution)
-                self.writeToDB(contribution)
+                };
+                createBalloon(contribution);
+                updateTagList(contribution);
+                writeToDB(contribution);            // may need to be renamed
             }
         }
     };
-
-    self.createBalloon = function(contribution) {
-        // this function creates the balloon, adds the text, positions it on the board
-
-        var balloon = $("<div class='balloon contribution'></div>");
-
-        balloon.attr('id', "contibution-" + contribution.id);
-        balloon.addClass('author-' + contribution.author);
-        $(contribution.tags).each(function() {
-            balloon.addClass(this);
-        })
-
-        balloon.hide() // initially hidden, we call show() with an effect later
-
-        // whole mess of stuff goes here (formatting, adding contribution.text, positioning, dragging, etc.)
-
-        $("#board").append(balloon);
-        balloon.show('puff', 'fast');
-
-        return balloon;
-    };
-
-    self.updateTagList = function(contribution) {
-        // this function handles the UI stuff for the list of tags on the sidebar
-        // then updates the list of user created tags with newly submitted tags
-
-        none_yet = $('#tags .none-yet');
-        if (none_yet.length > 0) {
-            none_yet.remove();
-        }
-        
-        list = $('#tags ul');
-        _.each(contribution.tags, function (tag) {
-            //klass = CommonBoard.keywordToClassName(tag);
-            li = list.find('.' + tag);                          // what's going on here?
-            if (li.length == 0) {
-                li = $('<li></li>');
-                li.text(tag);
-                li.addClass("tag-" + tag);
-                li.click(function() {
-                    // TODO set this up for filtering
-                    //self.toggleTag(tag);
-                })
-                list.append(li);
-            }
-        });
-
-
-        // _.difference(array, *others) 
-        // _.difference([1, 2, 3, 4, 5], [5, 2, 10]);
-        // => [1, 3, 4]
-        // update the saved list of tags
-        cumulativeTagArray = _.difference(contribution.tags, cumulativetagArray);
-
-
-    }
 
     return self;
 })();
