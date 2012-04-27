572d7ba175d9a6bfff3f570ce113e679d5508a0c
mikecules <michael.moncada@gmai.com>
2012-04-27 00:51:24 -0400

    Added custom tags and smarter UI for selecting previously used tags

--- js/washago.participant.js
+++ js/washago.participant.js
@@ -38,6 +38,10 @@ Washago.Participant = (function() {
             Sail.app.groupchat.addParticipantJoinedHandler(function(who, stanza) {
                 console.log(who + " joined...");
             });
+            
+            self.getLocations();
+            self.getTags();
+            self.initSearch();
         },
 
         'ui.initialized': function(ev) {
@@ -49,9 +53,9 @@ Washago.Participant = (function() {
                     author:"conferenceJoe",
                     text:"loriddy ipsumius bling la",
                     tags:["alpha", "beta"],
-                    id:"7582975289532",
-                    about:"poster_A",
-                    discourse_type:"question"
+                    id: "7582975289532",
+                    about: jQuery("#select-location").val(),
+                    discourse_type: jQuery('#radioType').val()
                 });
                 Sail.app.groupchat.sendEvent(sev);
             });
@@ -59,14 +63,63 @@ Washago.Participant = (function() {
     };
 
     self.getLocations = function() {
-        var dataStr ='{"tags":["collaboration", "embedded", "tablets", "bugs", "batman", "mobile", "science", "knowledge building","knowledge community", "inquiry"]}';
-        var data = jQuery.parseJSON(dataStr);
-        //jQuery.post();
-        
     };
     
     self.getTags = function() {
         
+        var dataStr ='{"tags":["collaboration", "embedded", "tablets", "bugs", "batman", "mobile", "science", "knowledge building","knowledge community", "inquiry"]}';
+        var data = jQuery.parseJSON(dataStr);
+        //jQuery.post();
+        var availableTags = jQuery("#tag-list-heading");
+        jQuery.each(data.tags, function(index, value) { 
+            //alert(index + ': ' + value);
+            availableTags.after('<li tag_id="' + value + '" data-theme="c"><a href="#page1">' + value + '</a></li>')
+        });
+        
+        jQuery('#tag-list').listview('refresh');
+        
+         self.initTagClick(jQuery('#tag-list li a'));
+        
+    };
+    self.initTagClick = function(obj) {
+        var availableTags = jQuery("#tag-list-heading");
+        jQuery(obj).click(function(){
+            var tagText = jQuery(this).text();
+            var tagButton = '<a href="#" class="tag_button" data-role="button" data-icon="delete" >' + tagText +'</a>';
+            jQuery('#chosen-tags').append( tagButton );
+            jQuery(this).parent().parent().parent().fadeOut(250);
+            jQuery('.tag_button').button();
+            jQuery('.tag_button').click(function(){
+                jQuery(this).fadeOut(250, function() { jQuery('[tag_id="' + tagText + '"]').fadeIn(250); jQuery(this).remove(); jQuery('#tag-list').listview('refresh');});
+                console.log('remove');
+            });
+            jQuery('.tag_button').buttonMarkup({inline: "true"});
+        });
+    };
+    
+    self.initSearch = function() {
+       $("div.ui-input-search").live("keyup", function(e){
+            if (e.which == 13) {
+                e.preventDefault();
+                var searchValueObj = jQuery('input[data-type="search"]');
+                var searchValue = jQuery(searchValueObj).val();
+                var tagButton = '<a href="#" class="tag_button" data-role="button" data-icon="delete" >' + searchValue +'</a>';
+                jQuery('#chosen-tags').append( tagButton );
+                jQuery('.tag_button').button();
+                jQuery('.tag_button').click(function(){
+                    jQuery(this).fadeOut(250, function() {  jQuery(this).remove(); });
+                    console.log('remove');
+                });
+                jQuery('.tag_button').buttonMarkup({inline: "true"});
+                jQuery(searchValueObj).val('');
+                jQuery(searchValueObj).trigger("change");
+                
+            }
+            //alert(e.which);
+            
+           
+            
+        });
     };
     
     return self;
