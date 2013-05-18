ab33dc2d7e6f580ad2d7bde7d76779937c7fb8e4
Armin Krauss <armin.krauss@utoronto.ca>
2013-05-18 19:01:56 -0400

    Basic grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+    jshint: {
+      all: ['Gruntfile.js', 'smartboard/js/*.js']
+    },
+    csslint: {
+      dev: {
+        options: {
+          'box-sizing': false,
+          'box-model': false,
+          'ids': false,
+          'important': false,
+          'shorthand': false,
+          'fallback-colors': false,
+          'compatible-vendor-prefixes': false,
+          'adjoining-classes': false,
+          'import': false
+        },
+        src: ['mobile/css/*.css', 'smartboard/css/*.css']
+      }
+    },
+    jsonlint: {
+      dev: {
+        src: ['./*.json' ]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-jsonlint');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'csslint', 'jsonlint']);
+  grunt.registerTask('lint', ['jshint', 'csslint', 'jsonlint']);
+};
