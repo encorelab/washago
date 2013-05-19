4f189ca7d2d7d14f324fb64645b076ba37b1a127
Matt Zukowski <matt@zukowski.ca>
2013-05-19 15:14:02 -0400

    started re-adding smartboard HTML and CSS

--- smartboard/js/smartboard.js
+++ smartboard/js/smartboard.js
@@ -1,1 +1,8 @@
-console.log('nothing here');
+(function () {
+  "use strict";
+
+  this.Washago = this.Washago || {};
+  this.Washago.Smartboard = this.Washago.Smartboard || {};
+  var smartboard = this.Washago.Smartboard;
+
+}).call(this);
