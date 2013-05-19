1a77aeeedc7cabb987385c96cb14a4291cb9b40e
colinmccann <colindmccann@gmail.com>
2013-05-19 13:04:57 -0400

    initial index-screen view finally set up

--- mobile/js/mobile.js
+++ mobile/js/mobile.js
@@ -3,16 +3,65 @@
 
 (function() {
   "use strict";
-
   var Washago = this.Washago || {};
+  this.Washago.Mobile = this.Washago.Mobile || {};
+  var app = this.Washago.Mobile;
 
-  Washago.Mobile = {};
- 
-  Washago.Mobile.setup = function () {
-     /* setup function */
+  app.keyCount = 0;
+  app.autoSaveTimer = window.setTimeout(function() { console.log("timer activated"); } ,10);
+  app.user = 'TODO';
 
-     /* loading config.json should go here */     
+  app.runState = null;
+  app.userState = null;
 
+  app.indexView = null;     // TODO - think about how necessary making these global is going to be
+
+  app.setup = function() {
+    /* setup function */
+
+    /* loading config.json should go here */
+
+    /* view/model setup */
+    // RUN
+    // USER
+    // MOBILE
+    app.indexView = new app.View.IndexView({
+      el: jQuery('#index-screen')
+    });
+
+    /* misc setup stuff */
+    jQuery().toastmessage({
+      position : 'middle-center'
+    });
+  };
+
+
+  app.autoSave = function(model, inputKey, inputValue, instantSave) {
+    app.keyCount++;
+    //console.log("saving stuff as we go at", app.keyCount);
+
+    // if (model.kind === 'buildOn') {
+    //   if (instantSave || app.keyCount > 9) {
+    //     // save to buildOn model to stay current with view
+    //     // app.buildOn = inputValue;
+    //     // save to contribution model so that it actually saves
+    //     // var buildOnArray = app.contribution.get('build_ons');
+    //     // var buildOnToUpdate = _.find(buildOnArray, function(b) {
+    //     //   return b.author === app.userData.account.login && b.published === false;
+    //     // });
+    //     // buildOnToUpdate.content = inputValue;
+    //     // app.contribution.set('build_ons',buildOnArray);
+    //     // app.contribution.save(null, {silent:true});
+    //     // app.keyCount = 0;
+    //   }
+    // } else {
+      if (instantSave || app.keyCount > 9) {
+        console.log('Saved');
+        //model.set(inputKey, inputValue);
+        //model.save(null, {silent:true});
+        app.keyCount = 0;
+      }
+    //}
   };
 
   this.Washago = Washago;
